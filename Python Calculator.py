3dc3ab764962f9f7abf0b6fcbfb4c4bd4daa8303
Neel <ksneel@hotmail.com>
2017-05-15 18:12:26 -0400

    Making changes to SecondCalcBranch Fixed syntax error in Print statement

--- Python Calculator.py
+++ Python Calculator.py
@@ -1,4 +1,4 @@
 print ("Hello World Calculator")
 print ("Checking for the second time")
 print ("Neel added third print statement")
-Print ("Making changes on the SecondCalcBranch")
+print ("Making changes on the SecondCalcBranch")
